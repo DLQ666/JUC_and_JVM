0a08942411c403c308f8fdc2cc70a5c471bc6882
DLQ <1114626450@qq.com>
2021-05-16 15:07:30 +0800

    GC垃圾回收

---
+++ JVM/src/com/dlq/jvmdemo/HelloGC.java
@@ -0,0 +1,19 @@
+package com.dlq.jvmdemo;
+
+/**
+ *@program: ZkDemo
+ *@description:
+ *@author: Hasee
+ *@create: 2021-03-09 13:15
+ */
+public class HelloGC {
+
+    public static void main(String[] args) throws InterruptedException {
+        /*long maxMemory = Runtime.getRuntime().maxMemory() ;//返回 Java 虚拟机试图使用的最大内存量。
+        long totalMemory = Runtime.getRuntime().totalMemory() ;//返回 Java 虚拟机中的内存总量。
+        System.out.println("MAX_MEMORY = " + maxMemory + "（字节）、" + (maxMemory / (double)1024 / 1024) + "MB");
+        System.out.println("TOTAL_MEMORY = " + totalMemory + "（字节）、" + (totalMemory / (double)1024 / 1024) + "MB");*/
+        System.out.println("***********HelloGC");
+        //Thread.sleep(Integer.MAX_VALUE);
+    }
+}
